30a88d5ad58ac9e06516144a6c09f365cab0dff1
Raheel Ahmad <Raheel.Ahmad1@ibm.com>
2020-05-28 17:52:44 +0500

    504 error fixed

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 import validUrl from 'valid-url';
-import { findEdges } from './util/child-process';
 (async () => {
 
   // Init the Express application
@@ -14,36 +13,21 @@ import { findEdges } from './util/child-process';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-   app.get("/filteredimage/", async (req, res) => {
+   app.get("/filteredimage/", async (req:express.Request, res:express.Response) => {
+    const url = req.query.image_url as string;
+
+    if (!url) {
+      res.sendStatus(400).send(`image_url is required`);
+    }
+
     try {
-      let { image_url } = req.query;
-      let { lower } = req.query;
-      let { upper } = req.query;
-
-      // Check if the user has entered any query parameter
-      if (!image_url) {
-        return res.status(422)
-          .send({error: 'Please specify image_url as query parameter'});
-      }
-
-      // If we have a query parameter, check if it is a valid url
-      if(!validUrl.isUri(image_url)){
-        return res.status(415).send({error: 'Please enter a valid url'});
-      }
-
-      // Process the image
-      let imgPath = await filterImageFromURL(image_url);
-      if(imgPath) {
-        const newPath = await findEdges(imgPath, lower, upper);
-        res.on('finish', () => deleteLocalFiles([imgPath, newPath.trim()]));
-        return res.status(200).sendFile(newPath.trim());
-      } else {
-        return res.status(500).send({error: 'Unable to elaborate your image'});
-      }
-    } catch {
-      return res.status(500).send({error: 'Unable to process your request'})
+      const filteredpath = await filterImageFromURL(url);
+      res.sendFile(filteredpath, () => deleteLocalFiles([filteredpath]));
+    } catch(error) {
+      res.sendStatus(422); 
     }
   });
+
 
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
@@ -66,7 +50,7 @@ import { findEdges } from './util/child-process';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
